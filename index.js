1992690c38ce66214e2801e576738c26b8ed1741
SanBu599 <112762794+SanBu599@users.noreply.github.com>
2024-02-21 13:54:06 +0000

    index.html change

--- index.js
+++ index.js
@@ -1,63 +1,69 @@
-// Change the current window shown by toggling the active class.
-$('.main-menu a').on('click', function (e) {
-
-	var activeClass = $(this).attr('href').substring(1);
-
-	setTimeout(function () {
-		$('.content.active').removeClass('active');
-
-		$('.' + activeClass).addClass('active');
-
-		$('html, body').animate({
-			scrollTop: $($('.' + activeClass)).offset().top
-		}, 500);
-
-	}, 300);
-
-	// Once a user clicks the portfolio link, animate the portfolio.
-	if (activeClass === 'portfolio') {
-
-		setTimeout(function () {
-
-			$('.filter-controls li')[0].click();
-
-		}, 300);
-	}
-
-});
-
-
-// Change the current active window based on the hash value in the url.
-var type = window.location.hash.substr(1);
-
-if (type) {
-	$('.content.active').removeClass('active');
-
-	$('.' + type).addClass('active');
-
-}
-
-// Magnific Popup init
-$('.popup-link').magnificPopup({
-	type: 'image'
-});
-
-// Adds active class to the current portfolio item selected.
-$('.filter-controls li').on('click', function () {
-
-	if (!$(this).hasClass('active')) {
-
-		$('.filter-controls li.active').removeClass('active');
-
-		$(this).addClass('active')
-
-	}
-
-});
-
-// Filterizr init for portfolio page.
-var options = {
-	/* check next step for available options */ };
-
-// Adjust the CSS selector to match the container where you set up your image gallery
-var filterizr = new Filterizr('.filter-container', options);
+// Check screen width immediately
+if ($(window).width() < 1000) {
+    $("#scrollbar").css("display", "none");
+    $("#about > div > div > ul > li > a").css("display", "inline-block");
+  }
+  
+  $(document).ready(function() {
+    var options = {
+    strings: ["a college student.", "a gamer.", "a frontend developer.", "Ka."],
+    typeSpeed: 40,
+    onComplete: function() {
+      $("#status").fadeTo("slow" , 1, function() {
+      // Animation complete.
+      });
+    },
+    startDelay: 850,
+    // backspacing speed
+    backSpeed: 50,
+    // time before backspacing
+    backDelay: 100,
+  }
+  
+  var typed = new Typed(".typed", options);
+    
+    // When collapse button is clicked
+    $("#sidebarCollapse").on("click", function() {
+      if ($("#sidebar").hasClass("active")) {
+        $("#content").css("margin-left", "12%");
+        $("#scrollbar").css("margin-left", "14%");
+      } else {
+        $("#content").css("margin-left", "80px");
+        $("#scrollbar").css("margin-left", "105px");
+      }
+      $("#sidebar").toggleClass("active");
+      $(this).toggleClass("active");
+    });
+  
+    $(window).scroll(function() {
+      if ($(window).scrollTop() != 0) {
+        $("#scrollbar").css("background", "rgba(52, 52, 54, 0.9)");
+      } else {
+        $("#scrollbar").css("background", "rgba(52, 52, 54, 0.2)");
+      }
+    });
+    
+    // When resizing occurs
+    $(window).resize(function () {
+      if ($(window).width() < 1500) {
+        if (!$("#sidebar").hasClass("active"))
+          $("#sidebarCollapse").trigger("click");
+        if ($(window).width() < 1000) {
+          $("#scrollbar").css("display", "none");
+        }
+        else
+          $("#scrollbar").css("display", "");
+        }
+      else {
+        $("#scrollbar").css("display", "");
+      }
+    });
+    
+    // When an anchor is clicked
+    $(document).on('click', 'a[href^="#"]', function (event) {
+      event.preventDefault();
+      $('html, body').animate({
+          scrollTop: $($.attr(this, 'href')).offset().top
+      }, 750);
+  });
+  });
